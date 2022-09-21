8cbd5080439fca96ea5f511bf8143a98f03546ee
thipp <thipp@Swamy>
2022-09-21 13:44:37 +0530

    Springboot project

--- ShoppingMallApplication/src/main/java/com/cg/customer/Customer.java
+++ ShoppingMallApplication/src/main/java/com/cg/customer/Customer.java
@@ -11,62 +11,50 @@ public class Customer {
 	private String name;
 	private String phone;
 	private String email;
-	private String orderid;
-
+	private Integer orderid;
 	public Customer() {
+		
 	}
-
-	public Customer(Integer id, String name, String phone, String email, String orderid) {
-
+	public Customer(Integer id, String name, String phone, String email, Integer orderid) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.phone = phone;
 		this.email = email;
 		this.orderid = orderid;
 	}
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getOrderid() {
+	public Integer getOrderid() {
 		return orderid;
 	}
-
-	public void setOrderid(String orderid) {
+	public void setOrderid(Integer orderid) {
 		this.orderid = orderid;
 	}
-
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", orderid="
